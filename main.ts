cbb078434757ea2b6c90da3e09ebe140657b3ebd
xsn0919 <59598087+xsn0919@users.noreply.github.com>
2026-06-01 11:59:13 +0800

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,5 @@
+// 放在 main.ts 文件的最顶部，用于快速测试平台功能
+Deno.cron("test-cron", "* * * * *", () => {Deno.cron(“test-cron",“ ;* * * * *& ”,()=祝辞{
+  console   控制台.log   日志("极简 Cron 测试，当前时间:", new   新    日期Date().toISOString());console   控制台.log   日志("极简 Cron 测试，当前时间:", new   新    日期Date().toISOString());
+});
+
